c50a219cfddc05c66a5e2bc3385496e128a8c6f7
Manuel Sanchez <manuelsanchez1100@gmail.com>
2016-07-07 00:27:21 -0500

    Spider implementation and item

---
+++ whackyspider/whackyspider/spiders/whackyspider.py
@@ -0,0 +1,25 @@
+from scrapy.spiders import Spider
+from scrapy.selector import Selector
+from ..items import WhackyHolidayItem
+# import sys
+
+class HolidaySpider(Spider):
+
+    name = "holidayspider"
+    allowed_domains = ["http://www.timeanddate.com/"]
+    start_urls = ["http://www.timeanddate.com/holidays/fun/"]
+
+    def parse(self, response):
+        # print(sys.path)
+        sel = Selector(response)
+        rows = sel.xpath('//div[@class="main-content-div"]/article/section[@class="article eight columns"]//tbody//tr')
+        items = []
+
+        for row in rows:
+            item = WhackyHolidayItem()
+            item['date'] = row.xpath('./th/text()').extract()
+            item['day'] = row.xpath('./td/text()').extract()
+            item['holiday'] = row.xpath('./td/a/text()').extract()
+            items.append(item)
+
+        return items

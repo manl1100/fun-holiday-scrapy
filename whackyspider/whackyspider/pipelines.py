4e91f54c98163bad58ac6dbd4dd97e4af8f89e45
Manuel Sanchez <manuelsanchez1100@gmail.com>
2016-07-08 01:16:48 -0500

    Format date

--- whackyspider/whackyspider/pipelines.py
+++ whackyspider/whackyspider/pipelines.py
@@ -5,10 +5,17 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.exceptions import DropItem
+import calendar
+import datetime
+
 
 class WhackyspiderPipeline(object):
     def process_item(self, item, spider):
         if not item['holiday'] and not item['day']:
             raise DropItem('Missing holiday and day of week')
         else:
+            month = str(list(calendar.month_abbr).index(item['date'].split()[0]))
+            day = item['date'].split()[1]
+            year = str(datetime.datetime.now().year)
+            item['date'] = '/'.join([month, day, year])
             return item
